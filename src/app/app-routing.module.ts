6aeb06102ce20593b9371436f417ea2f9865099c
Marija Shahrai <Marija.Shahrai@mef.dev>
2024-02-14 20:15:05 +0200

    add plugin required staff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlatformHelper } from '@natec/mef-dev-platform-connector';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = PlatformHelper.updatePluginsRoutes([
+  {
+    path:"",
+    children:[
+      {
+        path:"",
+        redirectTo:"test",
+        pathMatch:  'full',
+      },
+      {
+        path:"test",
+        component:  TestComponent
+      }
+    ]
+  }
+]);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'simple-plugin-example',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
